e0c92f623477223bb70da193bb62202687fc621c
sheydaashoorian <sheydaashoorian@gmail.com>
2023-05-28 14:52:19 +0330

    init project

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Faker\Factory as Faker;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication, DatabaseMigrations, DatabaseTransactions;
+
+    protected $faker;
+
+    /**
+     * Set up the test
+     */
+    public function setUp():void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
+
+    /**
+     * Reset the migrations
+     */
+    public function tearDown():void
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
+
+}
